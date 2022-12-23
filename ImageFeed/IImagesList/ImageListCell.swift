23e8d6c3f44b5a2a127c7f3162a436133af17a9b
RespectRus <respectspbrus@yandex.ru>
2022-12-23 15:04:57 +0300

    gradient

--- ImageFeed/IImagesList/ImageListCell.swift
+++ ImageFeed/IImagesList/ImageListCell.swift
@@ -2,4 +2,11 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     
+    @IBOutlet weak var dateLable: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var cellImage: UIImageView!
+    static let reuseIdentifier = "ImagesListCell"
+
+    @IBOutlet weak var gradientView: UIView!
+    
 }
