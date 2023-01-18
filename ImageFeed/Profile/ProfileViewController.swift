51ae446d0a8ba5235536f1303934f86b6bbaa0c1
RespectRus <respectspbrus@yandex.ru>
2023-01-18 10:52:40 +0300

    sprint_09

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var discriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton() {
+    }
+}
