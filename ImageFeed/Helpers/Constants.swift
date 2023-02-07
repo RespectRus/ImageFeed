15afb58fdc6336e57b611b84a7e0427b0b6f95e7
RespectRus <respectspbrus@yandex.ru>
2023-02-07 07:43:45 +0300

    likes

--- ImageFeed/Helpers/Constants.swift
+++ ImageFeed/Helpers/Constants.swift
@@ -4,7 +4,7 @@ enum Constants {
    static let accessKey = "InsrsXtDqqAnS87QhUbvESsvqurg0g5g8J72mLtpiCk"
    static let secretKey = "miNJ8ds5YaGvCyDkJQ3S3BKJF-UjUGG_cqHbb9wOuiY"
    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-   static let accessScope = "public+read_user+write_like"
+   static let accessScope = "public+read_user+write_likes"
    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
 }
 
