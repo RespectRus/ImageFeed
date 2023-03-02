ce5724f46dd957b26ed10a26ab33e2f311ce8334
Alexander Klementyev <113284257+RespectRus@users.noreply.github.com>
2023-03-02 11:25:02 +0300

    Delete AuthViewControllerDelegate.swift

--- ImageFeed/Auth/AuthViewControllerDelegate.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-protocol AuthViewViewControllerDelegate: AnyObject {
-    func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String)
-    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
-}
